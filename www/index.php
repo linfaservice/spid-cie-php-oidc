f5f9b031a1dd0e48fbfa90e9a946bdfcb4463452
sb003697 <sb003697@rmucsc.rm.unicatt.it@iam-proxy-1.fpg.local>
2024-07-24 17:45:23 +0200

    feat: get config from db

--- www/index.php
+++ www/index.php
@@ -8,9 +8,19 @@
     require("../lib/EndpointUserinfo.php");
     require("../lib/EndpointSessionEnd.php");
 
+    const CONFIG_FROM_DB = true;
     const CONFIG_FILE = "../spid-php-oidc.json";
 
     $config         = file_exists(CONFIG_FILE)? json_decode(file_get_contents(CONFIG_FILE), true) : array();
+
+    if(CONFIG_FROM_DB) {
+        $db_config_file = json_decode(file_get_contents('../database-config.json'), true);
+        $db_config = new PDO ($db_config_file['dsn'], $db_config_file['username'], $db_config_file['password']);
+        $getConfig = $db_config->prepare("SELECT oidc FROM spid_cie_php_config");
+        $getConfig->execute();
+        $config = json_decode($getConfig->fetch()['oidc'], true);
+    } 
+
     $db             = new Database($config['database']);
 
     $request_uri    = $_SERVER['REQUEST_URI'] ?? '';
